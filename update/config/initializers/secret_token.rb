1caf7ef60cd9ecaff734e1b4f930e67390659bb6
amjad <amjad@amjad-ThinkPad-T430.(none)>
2013-10-14 00:13:05 +0200

    added cms

---
+++ update/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Update::Application.config.secret_token = 'bc19e495c5362bd146bef024fbf840e62741f3d8fcdf4b5ac996ad5f8554159d12d81a5ad3f996ff268384717d02c32d3b712c66523b6c2b941bdd37db018e99'
