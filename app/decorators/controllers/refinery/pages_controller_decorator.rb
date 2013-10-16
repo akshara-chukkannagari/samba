ea8efc71a19dfba27960f89534a35b74012d4220
Amjad Saleem <asaleem@DENB0661.local>
2013-10-16 12:04:06 +0200

    added decorator to extend controller functionality

---
+++ app/decorators/controllers/refinery/pages_controller_decorator.rb
@@ -0,0 +1,9 @@
+Refinery::PagesController.class_eval do
+
+  before_filter :find_all_snippets, :only => [:home]
+  
+  protected 
+  def find_all_snippets
+    @snippets = Refinery::Snippets::Snippet.all
+  end
+end
