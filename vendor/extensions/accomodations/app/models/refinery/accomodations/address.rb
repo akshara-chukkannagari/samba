f1159fc57d891368cebea9e6e357e76fd8e315f5
Amjad Saleem <asaleem@DENB0661.local>
2013-10-18 14:48:50 +0200

    added validates to address

--- vendor/extensions/accomodations/app/models/refinery/accomodations/address.rb
+++ vendor/extensions/accomodations/app/models/refinery/accomodations/address.rb
@@ -4,7 +4,7 @@ module Refinery
 
       attr_accessible :street,:street_number, :town, :district, :state, :country, :lat, :lng, :alt, :accomodation_id, :position
 
-      validates :street, :presence => true, :uniqueness => true
+      validates :street,:street_number, :district, :state, :country, :presence => true
       belongs_to :accomodation
     end
   end
