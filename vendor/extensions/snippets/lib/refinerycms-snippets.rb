0c9de2e8c68cd8963b0bc89e814f8bc3a7eb9e17
Amjad Saleem <asaleem@DENB0661.local>
2013-10-23 12:02:47 +0200

    removed snippets

--- vendor/extensions/snippets/lib/refinerycms-snippets.rb
+++
@@ -1,2 +0,0 @@
-require 'refinery/snippets'
-require 'refinery/places'
--- vendor/extensions/snippets/spec/support/factories/refinery/snippets.rb
+++
@@ -1,7 +0,0 @@
-
-FactoryGirl.define do
-  factory :snippet, :class => Refinery::Snippets::Snippet do
-    sequence(:title) { |n| "refinery#{n}" }
-  end
-end
-
