61d826b5a4761f4141b972510b01cb32bbdfec83
amjad <amjad@amjad-ThinkPad-T430.(none)>
2013-10-27 14:01:28 +0100

    added language to samba content

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131026220436) do
+ActiveRecord::Schema.define(:version => 20131027125727) do
 
   create_table "refinery_accomodations", :force => true do |t|
     t.string   "name"
@@ -206,6 +206,7 @@ ActiveRecord::Schema.define(:version => 20131026220436) do
     t.integer  "position"
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
+    t.string   "language"
   end
 
   create_table "refinery_settings", :force => true do |t|
