6c9eb3e7897be8f7ba2f7f9d1ddd590d1f4b8cfb
amjad <amjad@amjad-ThinkPad-T430.(none)>
2013-10-27 14:27:47 +0100

    fixed refinery search

--- vendor/extensions/accomodations/app/models/refinery/accomodations/accomodation.rb
+++ vendor/extensions/accomodations/app/models/refinery/accomodations/accomodation.rb
@@ -2,7 +2,7 @@ module Refinery
   module Accomodations
     class Accomodation < Refinery::Core::BaseModel
       self.table_name = 'refinery_accomodations'
-
+      
       attr_accessible :name, :details, :teaser_image_id, :phone_number, :email, :url, :user_id, :available_from,:available_till,
       :sold_out, :position, :accomodation_type_id, :business_id, :language
       attr_accessible :price_attributes
@@ -13,9 +13,12 @@ module Refinery
       belongs_to :accomodation_type
       belongs_to :business
       has_one :price
-      
+          
       after_initialize :make_price
       accepts_nested_attributes_for  :price
+      
+      acts_as_indexed :fields => [:title, :details]
+        
       def make_price
         self.price ||= self.build_price
       end
